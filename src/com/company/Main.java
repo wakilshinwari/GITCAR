6eabc13d2917de7b1e56a5d3a2723cbc30f04c9e
wakilshinwari <shinwariabdulw@gmail.com>
2022-01-26 20:20:51 -0500

    here is your car now dont ask me to make changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,16 @@
 package com.company;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Main {
-    //the basic structure
-    CarBasics car = new CarBasics();
+    public static void main(String[] args) {
+
+        //the basic structure
+        CarBasics car = new CarBasics();
+        car.color = "green";
+        car.number = "789";
+        car.engine = "544444c";
+        car.door = "3";
+        System.out.println(car.color);
+    }
 }
