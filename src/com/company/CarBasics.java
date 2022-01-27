254057b97f5782f7fc5cd2741b76e338484bdde5
wakilshinwari <shinwariabdulw@gmail.com>
2022-01-26 19:25:51 -0500

    The basic of the car structure is ready without any feature

---
+++ src/com/company/CarBasics.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class CarBasics {
+    String color;
+    String engine;
+    String door;
+    String number;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Main {
+    //the basic structure
+    CarBasics car = new CarBasics();
+}
